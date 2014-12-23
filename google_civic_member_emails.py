507fc38ea2f2066e02cc94eb99314028590d9590
Rachel Shorey <rshorey@gmail.com>
2014-12-23 16:53:35 -0500

    google_civic_member_emails pulls emails for members based on district address

---
+++ google_civic_member_emails.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from google_api_key import API_KEY
+
+legislators = "legislators.json"
+
+base_url = "https://www.googleapis.com/civicinfo/v2/representatives?address=1%s&levels=country&key="+API_KEY
+
+
+emails = {}
+
+with open(legislators, 'r') as leg:
+	districts = json.loads(leg.read())
+	
+
+	for bioguide, d in districts.items():
+		address = "+".join([d["example_address"], d["example_city"], d["example_state"]]).replace(" ","+")
+		print address
+		url = base_url % address
+		r = requests.get(url)
+		try:
+			leg_json = r.json()["officials"]
+		except KeyError:
+			print("Legislator %s doesn't seem to be working" % bioguide)
+		else:
+			for person in leg_json:
+				if "emails" in person:
+					emails[person["name"]] = person["emails"][0]
+
+
+
