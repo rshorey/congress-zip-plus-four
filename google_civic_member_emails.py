a0a7b5e67b12d6ca28a097a44e897b500e83880a
Rachel Shorey <rshorey@gmail.com>
2014-12-24 13:59:20 -0500

    added correct senator addresses to legislators.json

--- google_civic_member_emails.py
+++ google_civic_member_emails.py
@@ -10,22 +10,23 @@ base_url = "https://www.googleapis.com/civicinfo/v2/representatives?address=1%s&
 emails = {}
 
 with open(legislators, 'r') as leg:
-	districts = json.loads(leg.read())
-	
+    districts = json.loads(leg.read())
+    
 
-	for bioguide, d in districts.items():
-		address = "+".join([d["example_address"], d["example_city"], d["example_state"]]).replace(" ","+")
-		print address
-		url = base_url % address
-		r = requests.get(url)
-		try:
-			leg_json = r.json()["officials"]
-		except KeyError:
-			print("Legislator %s doesn't seem to be working" % bioguide)
-		else:
-			for person in leg_json:
-				if "emails" in person:
-					emails[person["name"]] = person["emails"][0]
+    for bioguide, d in districts.items():
+        address = "+".join([d["example_address"], d["example_city"], d["example_state"]]).replace(" ","+")
+
+        url = base_url % address
+        r = requests.get(url)
+        try:
+            leg_json = r.json()["officials"]
+        except KeyError:
+            print address
+            print("Legislator %s, %s doesn't seem to be working" % (bioguide,d["name"]))
+        else:
+            for person in leg_json:
+                if "emails" in person:
+                    emails[person["name"]] = person["emails"][0]
 
 
 
