2d0c862fa8e6db0c8a69dc1f57acc97b1f7dd88c
Rachel Shorey <rshorey@gmail.com>
2015-01-05 15:27:53 -0500

    114th leg now in correct format

--- replace_outgoing.py
+++ replace_outgoing.py
@@ -1,5 +1,6 @@
 import csv
 import json
+from collections import OrderedDict
 
 
 changed_members = {}
@@ -9,19 +10,19 @@ with open("/home/rachel/work/contact-congress/support/update-to-114th/election_r
         if line["member_id"] != line["new_id"]:
             changed_members[line["member_id"]] = {"new_id":line["new_id"], "name":line["new_member"]}
 
-new_json = []
+new_json = {}
 
 with open("legislators.json","r") as leg:
     members = json.load(leg)
     for bioguide_id, info in members.items():
         if bioguide_id in changed_members:
             new_id = changed_members[bioguide_id]["new_id"]
-            new_dict = {new_id:info}
-            new_dict[new_id]["name"] = changed_members[bioguide_id]["name"]
-            new_json.append(new_dict)
+            info["name"] = changed_members[bioguide_id]["name"]
+            new_json[new_id] = info
         else:
-            new_json.append({bioguide_id:info})
+            new_json[bioguide_id] = info
 
+sorted_json = OrderedDict(sorted(new_json.items(), key=lambda t:t[0]))
 
 with open("114th_legislators.json","w") as new_leg:
-    new_leg.write(json.dumps(sorted(new_json), indent=4))
+    new_leg.write(json.dumps(sorted_json, indent=4))
